aeb5e66c8c2ce5cf27758dd32ada3cb6cd5711ab
Lahari-Urnani <lahariurnani0049@gmail.com>
2025-11-02 12:42:42 +0530

    Basic table with edit and delete

---
+++ BlazorIPaper/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorIPaper.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Course { get; set; }
+
+    }
+}
